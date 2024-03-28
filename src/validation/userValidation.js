f4bf01b7774e38777c60d8789e1dafaa34a01d92
RRAJ644 <riturajsingh.17248@gmail.com>
2024-03-28 23:51:30 +0530

    joi validation

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -1,10 +1,25 @@
-import { userSchema } from "../schema/index.js";
-import validateResponse from "../utils/validation.js"
-
+import { userSchema } from '../schema/index.js'
+import validateResponse from '../utils/validation.js'
 export const registerValidation = (req, res, next) => {
-  const validationError = validateObject(req.body, userSchema?.registerSchema);
+  const validationError = validateResponse(req.body, userSchema?.registerSchema)
   if (validationError) {
     return new Error(validationError)
   }
   next()
 }
+
+// export const registerParamsValidation = (req, res, next) => {
+//   const validationError = validateObject(req.params, userSchema?.registerSchema);
+//   if (validationError) {
+//     return new Error(validationError)
+//   }
+//   next()
+// }
+
+// export const registerQueryValidation = (req, res, next) => {
+//   const validationError = validateObject(req.query, userSchema?.registerSchema);
+//   if (validationError) {
+//     return new Error(validationError)
+//   }
+//   next()
+// }
